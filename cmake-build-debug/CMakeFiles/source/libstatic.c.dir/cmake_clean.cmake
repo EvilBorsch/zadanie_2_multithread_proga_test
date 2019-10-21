ee123cb45e161aab6a82996166a3c0bcc6f2357a
dmitrijgulacenkov <pingvin1959@yandex.ru>
2019-10-21 22:08:04 +0300

    try fix

---
+++ cmake-build-debug/CMakeFiles/source/libstatic.c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source/libstatic.c.dir/main.c.o"
+  "source/libstatic.c"
+  "source/libstatic.c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/source/libstatic.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
